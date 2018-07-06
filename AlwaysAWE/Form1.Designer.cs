89789a3464ed62798bd364dcd6720a426e3d5421
Albert <hvgmbdmg@gmail.com>
2018-07-06 13:47:19 +0800

    Clean Data instrinsic finish

---
+++ AlwaysAWE/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace AlwaysAWE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.SuspendLayout();
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.hScrollBar1.Location = new System.Drawing.Point(-2, 428);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.hScrollBar1.Size = new System.Drawing.Size(806, 22);
+            this.hScrollBar1.TabIndex = 0;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.hScrollBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+    }
+}
+
---
+++ CleanData/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CleanData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myMonth = new string[12] { "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12" };
+            string name = "";
+            for ( int m=0; m<12; m++ )
+            {
+                for( int d=1; d<32; d++ )
+                {
+                    if( d<10)
+                        name = "2018_" + myMonth[m] + "_0" + d;
+                    else
+                        name = "2018_" + myMonth[m] + "_" + d;
+
+                    LoadData(name);
+                }
+            }
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+
+        static bool LoadData( string name )
+        {
+            string filePath = @"C:\Users\albert_shen\Desktop\historyData\2018\Raw\Daily_" + name + ".csv";
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine(name + "does not exist!");
+                return false;
+            }
+
+            StreamReader file = new StreamReader(filePath);
+            int counter = 0;
+            int cleanCount = 0;
+            string line;
+            List<string> listA = new List<string>();
+            List<string> listB = new List<string>();
+            List<string> listC = new List<string>();
+            List<string> listD = new List<string>();
+            List<string> listE = new List<string>();
+            List<string> listF = new List<string>();
+
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] values = line.Split(',');
+                if ( values.Length >= 6 && values[1].Trim() == "TX")
+                {
+                    listA.Add(values[0].Trim());    //--- Date
+                    listB.Add(values[1].Trim());    //--- TX
+                    listC.Add(values[2].Trim());    //--- month
+                    listD.Add(values[3].Trim());    //--- hr,min,sec
+                    listE.Add(values[4].Trim());    //--- Point
+                    listF.Add(values[5].Trim());    //--- Volume
+                    //Console.WriteLine(listA[cleanCount] + "/" + listB[cleanCount] + "/" + listC[cleanCount] + "/" + 
+                    //                  listD[cleanCount] + "/" + listE[cleanCount] + "/" + listF[cleanCount] );
+                    cleanCount++;
+                }
+                counter++;
+            }
+
+            //--- Save Data
+            /*
+            var csv = new System.Text.StringBuilder();
+            for ( int i=0; i<cleanCount; i++ )
+            {
+                var newLine = $"{listA[i]},{listB[i]},{listC[i]},{listD[i]},{listE[i]},{listF[i]}";
+                csv.AppendLine(newLine);
+            }
+            File.WriteAllText(@"C:\Users\albert_shen\Desktop\AAA.csv", csv.ToString());
+            */
+
+
+            file.Close();
+            Console.WriteLine("There were {0} lines.", counter);
+            Console.WriteLine("There were {0} clean lines.", cleanCount);
+            return true;
+        }
+    }
+}
