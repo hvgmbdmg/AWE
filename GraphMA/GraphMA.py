d67e145b1865fae2dbe667fda34d8e877b8446f8
Albert <hvgmbdmg@gmail.com>
2018-07-16 13:25:02 +0800

    Add py part
    To download data from website

---
+++ GraphMA/GraphMA.py
@@ -0,0 +1,11 @@
+
+from twstock import Stock
+
+stock = Stock('2330')                             # 擷取台積電股價
+ma_p = stock.moving_average(stock.price, 5)       # 計算五日均價
+ma_c = stock.moving_average(stock.capacity, 5)    # 計算五日均量
+ma_p_cont = stock.continuous(ma_p)                # 計算五日均價持續天數
+ma_br = stock.ma_bias_ratio(5, 10)                # 計算五日、十日乖離值
+
+print( stock.price )
+print(stock.date)
