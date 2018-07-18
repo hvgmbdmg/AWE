19977aee8ade3a8664f3291a69fb97a21d8f553d
Albert <hvgmbdmg@gmail.com>
2018-07-18 16:13:57 +0800

    load history data

--- GraphMA/GraphMA.py
+++ GraphMA/GraphMA.py
@@ -4,26 +4,27 @@ from math import radians
 import numpy as np     # installed with matplotlib
 import matplotlib.pyplot as plt
 
-"""
-stock = Stock('2330')                             # 擷取台積電股價
+
+stock = Stock('2371')                             # 擷取台積電股價
 ma_p = stock.moving_average(stock.price, 5)       # 計算五日均價
 ma_c = stock.moving_average(stock.capacity, 5)    # 計算五日均量
 ma_p_cont = stock.continuous(ma_p)                # 計算五日均價持續天數
 ma_br = stock.ma_bias_ratio(5, 10)                # 計算五日、十日乖離值
-"""
 
-# print( stock.price )
-# print(stock.date)
+print( stock.price )
 
+stock.fetch(2018, 1)
+print(stock.data)
 
 # Try to load history data.
 # Create Function
 
 
-
+"""
 def main():
     x = np.arange(0, radians(1800), radians(12))
     plt.plot(x, np.cos(x), 'b')
     plt.show()
 
 main()
+"""
