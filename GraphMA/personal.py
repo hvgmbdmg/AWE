a2f976208e63a47499006533675e7c9d31f66207
Albert <hvgmbdmg@gmail.com>
2018-07-31 16:41:09 +0800

    rename, plan fileIO, finish load history data
    rename SaveStockData to aboutFile
    add some function & plan to do aboutFile,py function

---
+++ GraphMA/personal.py
@@ -0,0 +1,29 @@
+
+
+'''
+data structure:
+(price)
+    Code: XXXX
+    
+    priceKD   : int => MWD (000)~(111) let 1 means true.
+    priceMACD : int => MWD (000)~(111) let 1 means true.
+
+
+(capacity) 
+    Code: XXXX
+
+    capacityMA : int => (2W)WD (000)~(111) let 1 means true.
+    capacityMAvalue : 
+    C
+
+
+
+'''
+attentionList = ["1000","1001"]
+
+waitList = []
+
+
+
+
+
